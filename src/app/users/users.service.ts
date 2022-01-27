bb3645c1901b1eb81af2b3ac06f41d267fd3777e
Tuag <igortuag@gmail.com>
2022-01-27 10:22:01 -0300

    display user list

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
-import { User } from "./User";
+import { User } from "./user";
 
 @Injectable()
 export class UsersService {
