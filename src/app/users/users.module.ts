1cd71ff7ece951fdccead16e00f8211b5f9820d0
Tuag <igortuag@gmail.com>
2022-01-27 10:54:04 -0300

    add component user detail

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { UsersComponent } from "./users.component";
 import { UsersService } from "./users.service";
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, UserDetailComponent],
   providers: [UsersService],
 })
 export class UsersModule {}
