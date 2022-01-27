31a26813b44a997ee4ab27ff27420ea8b9cc369a
Tuag <igortuag@gmail.com>
2022-01-27 11:09:11 -0300

    add navigation to detail user

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  selector: "app-user-detail",
+  templateUrl: "./user-detail.component.html",
+  styleUrls: ["./user-detail.component.css"],
 })
-export class UserDetailComponent implements OnInit {
+export class UserDetailComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  id: Number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe((params) => {
+      this.id = params["id"];
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/users/users.routing.ts
+++ src/app/users/users.routing.ts
@@ -1,8 +1,12 @@
 import { ModuleWithProviders } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { UserDetailComponent } from "./user-detail/user-detail.component";
 import { UsersComponent } from "./users.component";
 
-const userRoutes = [{ path: "users", component: UsersComponent }];
+const userRoutes = [
+  { path: "users", component: UsersComponent },
+  { path: "users/:id", component: UserDetailComponent },
+];
 
 export const UserRoutingModule: ModuleWithProviders =
   RouterModule.forRoot(userRoutes);
