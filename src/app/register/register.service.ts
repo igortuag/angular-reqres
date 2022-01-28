c17f8fc4aa7106a5c691c6bfb07d9b0761ff9ac9
Tuag <igortuag@gmail.com>
2022-01-28 06:20:39 -0300

    add method to post form on register service

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Router } from "@angular/router";
 import formRegister from "./formRegister";
 
 @Injectable()
 export class RegisterService {
-  constructor() {}
+  constructor(private http: Http, private router: Router) {}
 
   register(form: formRegister) {
-    console.log("register ::>>", form);
+    return this.http
+      .post("https://reqres.in/api/register", {
+        email: form.email,
+        password: form.password,
+      })
+      .toPromise()
+      .then((response) => {
+        const data = response.json();
+        localStorage.setItem("token", data.token);
+        this.router.navigate(["/users"]);
+      })
+      .catch((error) => {
+        const errorData = error.json();
+        window.alert(`Register fail ${errorData.error}`);
+      });
   }
 }
